8e9119132ee982dda81570f109a0e08939f2079f
Zeeshan <zeeshan.mushtaq@crexed.com>
2023-12-04 22:45:49 +0500

    add api authentication

---
+++ admin/app/Http/Controllers/API/userController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password;
+use League\OAuth2\Server\Exception\OAuthServerException;
+
+/*
+    Author      : Zeeshan Mushtaq
+    Date        : 04-12-2023
+    Description : This  class will manage user authentication.
+*/
+
+class userController extends Controller
+{
+    // This function for authentication / login
+    public function authenticate(Request $request)
+    {
+        //Validate data
+        $validator = Validator::make($request->all(), [
+            'email' => ['required', 'email'],
+            'password'  =>  ['required', Password::min(8)]
+        ]);
+        //Send failed response if request is not valid
+        if ($validator->fails()) {
+            return $this->json_response('error', 'validation_failed', $validator->errors()->first(), 422);
+        }
+
+        try {
+            $credentials = $request->only('email', 'password');
+            $remember = request()->has('remember'); // Check if "remember" checkbox is checked
+
+            if (!Auth::attempt($credentials, $remember)) {
+                // Invalid credentials
+                throw new OAuthServerException('The user credentials were incorrect.', 6, 'invalid_credentials', 401);
+            }
+            $user = Auth::user();
+            $token = $user->createToken('auth_api', ['*'])->accessToken;
+            return $this->json_response('success', 'user_login', 'User login Successfully.', 200, $user, $token);
+        } catch (OAuthServerException $e) {
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+    }
+    // This function for register users
+    public function register(Request $request)
+    {
+        //Validate data
+        $data = $request->only('name', 'email', 'password_confirmation', 'password');
+        $validator = Validator::make($request->all(), [
+            'name' => ['required', 'min:3', 'string'],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password'  =>  ['required', Password::min(8)
+                ->letters()
+                ->mixedCase()
+                ->numbers()
+                ->symbols()
+                ->uncompromised()],
+            'password_confirmation' => ['required_with:password', 'same:password', Password::min(8)]
+        ]);
+
+        //Send failed response if request is not valid
+        if ($validator->fails()) {
+            return $this->json_response('error', 'validation_failed', $validator->errors()->first(), 422);
+        }
+        try {
+            $data = User::create([
+                'name' => $request->input('name'),
+                'email' => $request->input('email'),
+                'password' => Hash::make($request->input('password')),
+            ]);
+            // show created user
+            return $this->json_response('success', 'user_created', 'User Created Successfully', 200, $data);
+        } catch (\Exception $e) {
+            return response()->json($e->getMessage(), 404);
+        }
+    }
+}
--- admin/app/Http/Controllers/Controller.php
+++ admin/app/Http/Controllers/Controller.php
@@ -9,4 +9,18 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function json_response($type, $code, $message, $status, $data = '', $token = '', $indexname = 'user')
+    {
+        $response['type'] = $type;
+        $response['code'] = $code;
+        $response['message'] = $message;
+        if (!empty($data)) {
+            $response['data'][$indexname] = $data;
+        }
+        if (!empty($token)) {
+            $response['data']['token'] = $token;
+        }
+        return response()->json($response, $status);
+    }
 }

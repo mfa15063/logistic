1976edbdea2f243ca8b87b8b2653c06efd1eb957
M Furqan Afzal <muhammadfurqanafzalcr@gmail.com>
2024-01-22 22:34:37 +0500

    commit new

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import Portfolio from './pages/portfolio';
@@ -9,6 +9,7 @@ import Contact from './pages/contact';
 import FourZeroFour from './pages/404';
 import LayoutWithHeader from './layouts/layout-header';
 import LayoutWithOutHeader from './layouts/layout-noheader';
+import Profile from './pages/profile';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -19,6 +20,7 @@ root.render(
           <Route index element={<Home />} />
           <Route path='portfolio' element={<Portfolio />} />
           <Route path='about' element={<About />} />
+          <Route path='profile' element={<Profile/>} />
           {/* <Route path='contact' element={<Contact />} /> */}
         </Route>
         <Route path='*' element={<LayoutWithOutHeader />} >
---
+++ src/pages/profile.jsx
@@ -0,0 +1,106 @@
+import React, { useEffect } from "react";
+import "./profile.scss";
+import SignUp from "../components/signup";
+import ProfileComponent from "../components/profile";
+
+const Profile = () => {
+  useEffect(() => {
+    let header = document.getElementById("header");
+    if (header) header.classList.add("profile-header");
+    let getSidebar = document.querySelector(".profile-page nav");
+    let getToggle = document.querySelectorAll(".profile-page .toggle");
+    let handleToggleClick = () => {
+      if (getSidebar) getSidebar.classList.toggle("active");
+    };
+    getToggle.forEach((toggle) => {
+      toggle.addEventListener("click", handleToggleClick);
+    });
+
+    return () => {
+      getToggle.forEach((toggle) => {
+        toggle.removeEventListener("click", handleToggleClick);
+      });
+    };
+  }, []);
+  return (
+    <div className="profile-page">
+      <div className="toggle ioooi" style={{ display: "none" }}>
+        <i className="fas fa-bars" />
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a className="toggle">
+              <span className="icon">
+                <i className="fas fa-bars" />
+              </span>
+              <span className="title">Menu</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span className="icon">
+                <i className="fas fa-home" />
+              </span>
+              <span className="title">Home</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span className="icon">
+                <i className="fas fa-user" />
+              </span>
+              <span className="title">Profile</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span className="icon">
+                <i className="fas fa-envelope" />
+              </span>
+              <span className="title">Messages</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span className="icon">
+                <i className="fas fa-info" />
+              </span>
+              <span className="title">Help</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span className="icon">
+                <i className="fas fa-cog" />
+              </span>
+              <span className="title">Setting</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span className="icon">
+                <i className="fas fa-lock" />
+              </span>
+              <span className="title">Password</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span className="icon">
+                <i className="fas fa-sign-out-alt" />
+              </span>
+              <span className="title">Sign Out</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div className="profile-right">
+        {/* <SignUp/> */}
+        <ProfileComponent/>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;

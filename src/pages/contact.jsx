6c2cd12ddb27fa2bebe49f6aecbf177158a6b23b
mfa15063 <muhammadfurqanafzalcr@gmail.com>
2024-04-07 11:03:22 +0500

    update contact

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,73 +1,96 @@
 let Contact = () => {
     return (
         <>
-            {/* <!-- Contact Section--> */}
-            <section className="page-section" id="contact">
-                <div className="container">
-                    {/* <!-- Contact Section Heading--> */}
-                    <h2 className="page-section-heading text-center text-uppercase text-secondary mb-0">Contact Me</h2>
-                    {/* <!-- Icon Divider--> */}
-                    <div className="divider-custom">
-                        <div className="divider-custom-line"></div>
-                        <div className="divider-custom-icon"><i className="fas fa-star"></i></div>
-                        <div className="divider-custom-line"></div>
-                    </div>
-                    {/* <!-- Contact Section Form--> */}
-                    <div className="row justify-content-center">
-                        <div className="col-lg-8 col-xl-7">
-                            {/* <!-- * * * * * * * * * * * * * * *--> */}
-                            {/* <!-- To make this form functional, sign up at--> */}
-                            {/* <!-- https://startbootstrap.com/solution/contact-forms--> */}
-                            {/* <!-- to get an API token!--> */}
-                            <form id="contactForm">
-                                {/* <!-- Name input--> */}
-                                <div className="form-floating mb-3">
-                                    <input className="form-control" id="name" type="text" placeholder="Enter your name..." />
-                                    <label htmlFor="name">Full name</label>
-                                    <div className="invalid-feedback">A name is required.</div>
-                                </div>
-                                {/* <!-- Email address input--> */}
-                                <div className="form-floating mb-3">
-                                    <input className="form-control" id="email" type="email" placeholder="name@example.com" />
-                                    <label htmlFor="email">Email address</label>
-                                    <div className="invalid-feedback">An email is required.</div>
-                                    <div className="invalid-feedback">Email is not valid.</div>
-                                </div>
-                                {/* <!-- Phone number input--> */}
-                                <div className="form-floating mb-3">
-                                    <input className="form-control" id="phone" type="tel" placeholder="(123) 456-7890" />
-                                    <label htmlFor="phone">Phone number</label>
-                                    <div className="invalid-feedback">A phone number is required.</div>
-                                </div>
-                                {/* <!-- Message input--> */}
-                                <div className="form-floating mb-3">
-                                    <textarea className="form-control" id="message" type="text" placeholder="Enter your message here..." style={{height:  "10rem"}} defaultValue={""} />
-
-                                    <label htmlFor="message">Message</label>
-                                    <div className="invalid-feedback">A message is required.</div>
-                                </div>
-                                {/* <!-- Submit success message--> */}
-                                {/* <!-- This is what your users will see when the form--> */}
-                                {/* <!-- has successfully submitted--> */}
-                                <div className="d-none" id="submitSuccessMessage">
-                                    <div className="text-center mb-3">
-                                        <div className="fw-bolder">Form submission successful!</div>
-                                        To activate this form, sign up at
-                                        <br />
-                                        <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
-                                    </div>
-                                </div>
-                                {/* <!-- Submit error message--> */}
-                                {/* <!-- This is what your users will see when there is--> */}
-                                {/* <!-- an error submitting the form--> */}
-                                <div className="d-none" id="submitErrorMessage"><div className="text-center text-danger mb-3">Error sending message!</div></div>
-                                {/* <!-- Submit Button--> */}
-                                <button className="btn btn-primary btn-xl disabled" id="submitButton" type="submit">Send</button>
-                            </form>
-                        </div>
+            {/* ======= #main ======= */}
+            <main id="main">
+                {/* ======= Breadcrumbs ======= */}
+                <div className="breadcrumbs d-flex align-items-center"
+                     style={{backgroundImage: 'url("/assets/img/breadcrumbs-bg.jpg")'}}>
+                    <div className="container position-relative d-flex flex-column align-items-center">
+                        <h2>Contact</h2>
+                        <ol>
+                            <li><a href="index.html">Home</a></li>
+                            <li>Contact</li>
+                        </ol>
                     </div>
                 </div>
-            </section>
+                {/* End Breadcrumbs */}
+                {/* ======= Contact Section ======= */}
+                <section id="contact" className="contact">
+                    <div className="container" data-aos="fade-up" data-aos-delay={100}>
+                        <div className="row gy-4">
+                            <div className="col-lg-6">
+                                <div
+                                    className="info-item  d-flex flex-column justify-content-center align-items-center">
+                                    <i className="bi bi-map"/>
+                                    <h3>Our Address</h3>
+                                    <p>A108 Adam Street, New York, NY 535022</p>
+                                </div>
+                            </div>
+                            {/* End Info Item */}
+                            <div className="col-lg-3 col-md-6">
+                                <div className="info-item d-flex flex-column justify-content-center align-items-center">
+                                    <i className="bi bi-envelope"/>
+                                    <h3>Email Us</h3>
+                                    <p>contact@example.com</p>
+                                </div>
+                            </div>
+                            {/* End Info Item */}
+                            <div className="col-lg-3 col-md-6">
+                                <div
+                                    className="info-item  d-flex flex-column justify-content-center align-items-center">
+                                    <i className="bi bi-telephone"/>
+                                    <h3>Call Us</h3>
+                                    <p>+1 5589 55488 55</p>
+                                </div>
+                            </div>
+                            {/* End Info Item */}
+                        </div>
+                        <div className="row gy-4 mt-1">
+                            <div className="col-lg-6 ">
+                                <iframe
+                                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
+                                    frameBorder={0} style={{border: 0, width: '100%', height: 384}} allowFullScreen/>
+                            </div>
+                            {/* End Google Maps */}
+                            <div className="col-lg-6">
+                                <form action="forms/contact.php" method="post" role="form" className="php-email-form">
+                                    <div className="row gy-4">
+                                        <div className="col-lg-6 form-group">
+                                            <input type="text" name="name" className="form-control" id="name"
+                                                   placeholder="Your Name" required/>
+                                        </div>
+                                        <div className="col-lg-6 form-group">
+                                            <input type="email" className="form-control" name="email" id="email"
+                                                   placeholder="Your Email" required/>
+                                        </div>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="text" className="form-control" name="subject" id="subject"
+                                               placeholder="Subject" required/>
+                                    </div>
+                                    <div className="form-group">
+                                        <textarea className="form-control" name="message" rows={5} placeholder="Message"
+                                                  required defaultValue={""}/>
+                                    </div>
+                                    <div className="my-3">
+                                        <div className="loading">Loading</div>
+                                        <div className="error-message"/>
+                                        <div className="sent-message">Your message has been sent. Thank you!</div>
+                                    </div>
+                                    <div className="text-center">
+                                        <button type="submit">Send Message</button>
+                                    </div>
+                                </form>
+                            </div>
+                            {/* End Contact Form */}
+                        </div>
+                    </div>
+                </section>
+                {/* End Contact Section */}
+            </main>
+            {/* End #main */}
+
         </>
     );
 }
